72e04bfd3ed5f6fc7f5061c6c7dcfdee4110aa3c
JochiSt <jochen.steinmann@web.de>
2025-02-18 16:30:06 +0100

    almost started the conversion script from scratch to include more features of the updated HLS4ML

---
+++ SimpleAbsNetwork/convert2FPGA.py
@@ -0,0 +1,65 @@
+"""
+    script to convert the network using HLS4ML in HDL code
+"""
+
+import os
+os.environ["PATH"] = "/opt/Xilinx/Vitis_HLS/2023.2/bin/:" + os.environ["PATH"]
+
+# Convert the model to FPGA firmware with hls4ml
+# Make an hls4ml config & model
+import hls4ml
+import json
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# use the already trained model
+from tensorflow.keras.models import load_model
+
+model = load_model("models/SimpleLinearModel.h5")
+
+# create configuration for each layer
+config = hls4ml.utils.config_from_keras_model(model, granularity='name', backend='Vitis')  
+print("-----------------------------------")
+from plotting import print_dict
+print_dict(config)
+print("-----------------------------------")
+
+################################################################################
+from simple_linear_model import create_datasets
+x_train, x_test, x_validate, y_train, y_test, y_validate = create_datasets(1000)
+x_test = np.concatenate((x_train, x_test, x_validate))
+y_test = np.concatenate((y_train, y_test, y_validate))
+
+from hls4ml.model.profiling import numerical, get_ymodel_keras
+
+for layer in config['LayerName'].keys():
+    config['LayerName'][layer]['Trace'] = True
+    
+hls_model = hls4ml.converters.convert_from_keras_model(
+    model, hls_config=config, output_dir='model_1/hls4ml_prj_2', part='xcu250-figd2104-2L-e'
+)
+
+# evaluate the performance of the neural network
+hls_model.compile()
+hls4ml_pred, hls4ml_trace = hls_model.trace(x_test[:1000])
+keras_trace = get_ymodel_keras(model, x_test[:1000])
+y_hls = hls_model.predict(x_test)
+
+print("Keras layer 'layer_1', first sample:")
+print(keras_trace['layer_1'][0])
+print("hls4ml layer 'layer_1', first sample:")
+print(hls4ml_trace['layer_1'][0])
+
+
+plots = numerical(
+    model=model,            # Keras model
+    hls_model=hls_model,    # HLS model
+    X=x_test                # test data
+    )
+plt.show()
+
+hls4ml.utils.plot_model(hls_model, show_shapes=True, show_precision=True, to_file="plots/HLSmodel.png")
+
+
