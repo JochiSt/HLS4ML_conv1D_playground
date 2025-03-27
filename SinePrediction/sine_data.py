49d09354e72a0fd5f7c201f91006d7403866b933
JochiSt <jochen.steinmann@web.de>
2025-03-27 15:42:47 +0100

    added a function to create the datasets (splitted or not)

--- SinePrediction/sine_data.py
+++ SinePrediction/sine_data.py
@@ -17,6 +17,38 @@ def generate_data(NSAMPLES = 10, NHISTO = 100, freq = 10, t_sample = 0.001):
     return np_sin_windowed, np_times_windowed, prediction
     
     
+def create_datasets(SAMPLES=10000, split=True):
+    # We'll use 60% of our data for training and 20% for testing.
+    # The remaining 20% will be used for validation. Calculate the indices of
+    # each section.
+    TRAIN_SPLIT = int(0.6 * SAMPLES)
+    TEST_SPLIT = int(0.2 * SAMPLES + TRAIN_SPLIT)
+
+    # generate the training data
+    y_values, _, x_values = generate_data(NSAMPLES=SAMPLES)
+
+    # convert data into numpy arrays
+    x_values = np.array(x_values)  # waveforms
+    y_values = np.array(y_values)  # parameters
+
+    # Use np.split to chop our data into three parts.
+    # The second argument to np.split is an array of indices where the data
+    # will be split. We provide two indices, so the data will be divided into
+    # three chunks.
+    if split:
+        x_train, x_test, x_validate = np.split(x_values, [TRAIN_SPLIT, TEST_SPLIT])
+        y_train, y_test, y_validate = np.split(y_values, [TRAIN_SPLIT, TEST_SPLIT])
+    else:
+        x_train = x_values
+        x_test = x_values
+        x_validate = x_values
+        
+        y_train = y_values
+        y_test = y_values
+        y_validate = y_values
+
+    return x_train, x_test, x_validate, y_train, y_test, y_validate
+    
 if __name__ == "__main__":
     
     np_sin, np_times, prediction = generate_data()
